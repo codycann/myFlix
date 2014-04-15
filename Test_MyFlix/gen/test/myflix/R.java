f52ba6eabd98887f154f0f9bd0560828a6e982fb
Alec Nathan <alecnathan@gmail.com>
2014-04-15 18:05:36 -0400

    added login (not connected to db yet)

--- Test_MyFlix/gen/test/myflix/R.java
+++ Test_MyFlix/gen/test/myflix/R.java
@@ -34,60 +34,83 @@ public final class R {
         public static final int ic_launcher=0x7f020005;
     }
     public static final class id {
-        public static final int VideoView=0x7f090011;
-        public static final int action_collection=0x7f090013;
-        public static final int action_search=0x7f090012;
-        public static final int action_settings=0x7f090015;
-        public static final int action_video=0x7f090014;
-        public static final int border_layout=0x7f09000c;
-        public static final int button1=0x7f09000a;
-        public static final int editText1=0x7f090001;
-        public static final int editText2=0x7f090002;
+        public static final int VideoView=0x7f090018;
+        public static final int action_collection=0x7f09001a;
+        public static final int action_forgot_password=0x7f09001d;
+        public static final int action_search=0x7f090019;
+        public static final int action_settings=0x7f09001c;
+        public static final int action_video=0x7f09001b;
+        public static final int border_layout=0x7f090013;
+        public static final int button1=0x7f090011;
+        public static final int editText1=0x7f090008;
+        public static final int editText2=0x7f090009;
+        public static final int email=0x7f090004;
         public static final int gridview1=0x7f090000;
-        public static final int gridview_genre=0x7f09000b;
-        public static final int movie_poster=0x7f09000d;
-        public static final int movie_rating=0x7f09000f;
-        public static final int movie_title=0x7f09000e;
-        public static final int radio0=0x7f090008;
-        public static final int radio1=0x7f090009;
-        public static final int radioGroup1=0x7f090006;
-        public static final int spinner1=0x7f090003;
-        public static final int spinner2=0x7f090004;
-        public static final int spinner3=0x7f090005;
-        public static final int textView1=0x7f090007;
-        public static final int viewpager=0x7f090010;
+        public static final int gridview_genre=0x7f090012;
+        public static final int login=0x7f090006;
+        public static final int login_form=0x7f090003;
+        public static final int login_status=0x7f090001;
+        public static final int login_status_message=0x7f090002;
+        public static final int movie_poster=0x7f090014;
+        public static final int movie_rating=0x7f090016;
+        public static final int movie_title=0x7f090015;
+        public static final int password=0x7f090005;
+        public static final int radio0=0x7f09000f;
+        public static final int radio1=0x7f090010;
+        public static final int radioGroup1=0x7f09000d;
+        public static final int sign_in_button=0x7f090007;
+        public static final int spinner1=0x7f09000a;
+        public static final int spinner2=0x7f09000b;
+        public static final int spinner3=0x7f09000c;
+        public static final int textView1=0x7f09000e;
+        public static final int viewpager=0x7f090017;
     }
     public static final class layout {
         public static final int activity_connect=0x7f030000;
-        public static final int activity_search=0x7f030001;
-        public static final int fragment_grid=0x7f030002;
-        public static final int movie_view=0x7f030003;
-        public static final int moviecollection=0x7f030004;
-        public static final int videoview_main=0x7f030005;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_search=0x7f030002;
+        public static final int fragment_grid=0x7f030003;
+        public static final int movie_view=0x7f030004;
+        public static final int moviecollection=0x7f030005;
+        public static final int videoview_main=0x7f030006;
     }
     public static final class menu {
         public static final int activity_main_actions=0x7f080000;
         public static final int connect=0x7f080001;
-        public static final int search=0x7f080002;
+        public static final int login=0x7f080002;
+        public static final int search=0x7f080003;
     }
     public static final class string {
         public static final int action_collection=0x7f050005;
+        public static final int action_forgot_password=0x7f05000d;
         public static final int action_search=0x7f050004;
         public static final int action_settings=0x7f050001;
+        public static final int action_sign_in_register=0x7f05000b;
+        public static final int action_sign_in_short=0x7f05000c;
         public static final int action_video=0x7f050006;
-        public static final int actor_hint=0x7f05000b;
+        public static final int actor_hint=0x7f050016;
         public static final int app_name=0x7f050000;
-        public static final int genre_prompt=0x7f05000c;
+        public static final int error_field_required=0x7f050012;
+        public static final int error_incorrect_password=0x7f050011;
+        public static final int error_invalid_email=0x7f05000f;
+        public static final int error_invalid_password=0x7f050010;
+        public static final int genre_prompt=0x7f050017;
         public static final int hello_world=0x7f050002;
+        public static final int login_progress_signing_in=0x7f05000e;
         public static final int poster_desc=0x7f050003;
-        public static final int rating_prompt=0x7f05000d;
-        public static final int rating_radio=0x7f050009;
-        public static final int runtime_prompt=0x7f05000e;
-        public static final int search=0x7f05000f;
-        public static final int sort=0x7f050010;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f050009;
+        public static final int prompt_password=0x7f05000a;
+        public static final int rating_prompt=0x7f050018;
+        public static final int rating_radio=0x7f050014;
+        public static final int runtime_prompt=0x7f050019;
+        public static final int search=0x7f05001a;
+        public static final int sort=0x7f05001b;
+        public static final int title_activity_login=0x7f050008;
         public static final int title_activity_search=0x7f050007;
-        public static final int title_hint=0x7f05000a;
-        public static final int title_radio=0x7f050008;
+        public static final int title_hint=0x7f050015;
+        public static final int title_radio=0x7f050013;
     }
     public static final class style {
         /** 
@@ -113,9 +136,12 @@ public final class R {
          */
         public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-   		<item name="android:windowNoTitle">true</item> 
+
+           All customizations that are NOT specific to a particular API-level can go here. 
+   		<item name="android:windowNoTitle">true</item>
+        
          */
         public static final int AppTheme=0x7f070001;
+        public static final int LoginFormContainer=0x7f070002;
     }
 }
